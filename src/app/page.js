9dce30baf5f110ae5c7156b26cc5a068571795b4
Rafiadnan0666 <fn234561@gmail.com>
2025-04-22 13:49:27 +0700

    ok

--- src/app/page.js
+++ src/app/page.js
@@ -16,14 +16,23 @@ export default function Home() {
   return (
     <div>
       <title>Rafi Adnan Portoflio</title>
-      {/* <link
+      <link
       className="rounded"
       rel="icon"
       href="https://fiverr-res.cloudinary.com/t_profile_original,q_auto,f_auto/attachments/profile/photo/5807b862bf1790ac6b1f82ab75d1be73-1743593947676/af264c2c-8fbc-4003-a5b3-d0b46292c8f8.png"
       type="image/x-icon"
-    /> */}
+    />
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4320225638717558"
      crossOrigin="anonymous"></script>
+     <script src='https://storage.ko-fi.com/cdn/scripts/overlay-widget.js'></script>
+<script>
+  kofiWidgetOverlay.draw('gregsea', {
+    'type': 'floating-chat',
+    'floating-chat.donateButton.text': 'Support me',
+    'floating-chat.donateButton.background-color': '#00b9fe',
+    'floating-chat.donateButton.text-color': '#fff'
+  });
+</script>
       <House />
       <Projects />
       <Analytics />
