a896db0fd13000dd6e22c5c5c32f6467a0b27ab4
Rafiadnan0666 <fn234561@gmail.com>
2025-04-23 18:38:59 +0700

    \

--- src/app/page.js
+++ src/app/page.js
@@ -9,14 +9,14 @@ import Script from "next/script";
 export default function Home() {
   return (
     <>
-      <Head>
+      <>
         <title>Rafi Adnan Portfolio</title>
         <link
           rel="icon"
           href="https://fiverr-res.cloudinary.com/t_profile_original,q_auto,f_auto/attachments/profile/photo/5807b862bf1790ac6b1f82ab75d1be73-1743593947676/af264c2c-8fbc-4003-a5b3-d0b46292c8f8.png"
           type="image/x-icon"
         />
-      </Head>
+      </>
 
       {/* Google AdSense */}
       <Script
