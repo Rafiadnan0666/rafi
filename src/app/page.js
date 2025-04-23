f5446739ed0adad45f5fb0e7c16f76490145210e
Rafiadnan0666 <fn234561@gmail.com>
2025-04-23 18:28:12 +0700

    gugel

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 "use client";
+
 import { Analytics } from "@vercel/analytics/react";
 import House from "./House/House";
 import Projects from "./Projects/page";
-
 import Head from "next/head";
+import Script from "next/script";
 
 export default function Home() {
   return (
@@ -15,37 +16,45 @@ export default function Home() {
           href="https://fiverr-res.cloudinary.com/t_profile_original,q_auto,f_auto/attachments/profile/photo/5807b862bf1790ac6b1f82ab75d1be73-1743593947676/af264c2c-8fbc-4003-a5b3-d0b46292c8f8.png"
           type="image/x-icon"
         />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6910980494327419"
-          crossOrigin="anonymous"></script>
-
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-V16NP44WSM"></script>
-        <script dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-V16NP44WSM');
-          `,
-        }} />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              const script = document.createElement('script');
-              script.src = 'https://storage.ko-fi.com/cdn/scripts/overlay-widget.js';
-              script.onload = () => {
-                kofiWidgetOverlay.draw('gregsea', {
-                  'type': 'floating-chat',
-                  'floating-chat.donateButton.text': 'Support me',
-                  'floating-chat.donateButton.background-color': '#00b9fe',
-                  'floating-chat.donateButton.text-color': '#fff'
-                });
-              };
-              document.head.appendChild(script);
-            `,
-          }}
-        />
       </Head>
+
+      {/* Google AdSense */}
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6910980494327419"
+        crossOrigin="anonymous"
+      />
+
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-V16NP44WSM"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-V16NP44WSM');
+        `}
+      </Script>
+
+      {/* Ko-Fi Floating Widget */}
+      <Script id="kofi-widget" strategy="afterInteractive">
+        {`
+          const script = document.createElement('script');
+          script.src = 'https://storage.ko-fi.com/cdn/scripts/overlay-widget.js';
+          script.onload = () => {
+            kofiWidgetOverlay.draw('gregsea', {
+              'type': 'floating-chat',
+              'floating-chat.donateButton.text': 'Support me',
+              'floating-chat.donateButton.background-color': '#00b9fe',
+              'floating-chat.donateButton.text-color': '#fff'
+            });
+          };
+          document.head.appendChild(script);
+        `}
+      </Script>
 
       <main>
         <House />
